efde07a875543cb78cbd3d8d4af842b746b24a02
nurahsanadzim <nurahsanadzim@gmail.com>
2025-05-01 09:24:15 +0700

    change from CrudRepository to JpaRepository

--- src/main/java/com/example/crud/repository/PersonRepository.java
+++ src/main/java/com/example/crud/repository/PersonRepository.java
@@ -1,8 +1,9 @@
 package com.example.crud.repository;
 
 import com.example.crud.entity.Person;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
