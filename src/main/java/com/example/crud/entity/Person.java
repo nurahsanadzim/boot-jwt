433052d60aa141b2739dc1ad2231ab96d4b3625e
nurahsanadzim <nurahsanadzim@gmail.com>
2025-05-01 08:13:27 +0700

    remove unused import

--- src/main/java/com/example/crud/entity/Person.java
+++ src/main/java/com/example/crud/entity/Person.java
@@ -1,6 +1,5 @@
 package com.example.crud.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
