c0ab7eec5ab1eb8d76ffecddb050fa6082621232
nurahsanadzim <nurahsanadzim@gmail.com>
2025-05-01 09:07:54 +0700

    make unique contraint for username and field each

--- src/main/java/com/example/crud/entity/Person.java
+++ src/main/java/com/example/crud/entity/Person.java
@@ -1,11 +1,11 @@
 package com.example.crud.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
@@ -16,12 +16,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "person", uniqueConstraints = @UniqueConstraint(columnNames = {"username", "email"}))
+@Table(name = "person")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String username;
+    @Column(unique = true)
     private String email;
     private String password;
 }
