a72c9e47f9f241bf942177f29c7f8aede4bdebb1
nurahsanadzim <nurahsanadzim@gmail.com>
2025-05-05 05:06:55 +0700

    trying test class

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,37 +1,28 @@
 package com.example.jwt;
 
-// import com.example.jwt.security.JwtUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.jwt.security.JwtUtil;
 // import io.jsonwebtoken.Claims;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-class JwtApplicationTests {
 
-    // private final JwtUtil jwtUtil;
+@SpringBootTest
+public class JwtApplicationTests {
 
-    // @Autowired
-    // public JwtApplicationTests(JwtUtil jwtUtil) {
-    //     this.jwtUtil = jwtUtil;
-    // }
+    @Autowired
+    private JwtUtil jwtUtil;
 
-    // @Test
-    // void testGenerateToken() {
-    //     // Generate a token for a test user
-    //     String token = jwtUtil.generateToken("testUser");
+    // static JwtUtil jwtUtil;
 
-    //     // Print the generated token to the console (for debugging purposes)
-    //     System.out.println("Generated JWT Token: " + token);
+    @Test
+    void jwtUtilDebug() {
+        System.out.println("#### START DEBUG ####");
+        
+        String test = jwtUtil.generateToken("debug username");
 
-    //     // Optionally, extract claims from the generated token and print them
-    //     // Claims claims = jwtUtil.extractClaims(token);
-    //     String claims = jwtUtil.extractClaims(token);
-    //     System.out.println("Extracted Claims: " + claims);
-
-    //     // You can print other debug info, such as the token's expiration time
-    //     // System.out.println("Token Expiration: " + claims.getExpiration());
-    // }
-
-    // Optional: Add more tests to check other functionality of JwtUtil if needed
+        System.out.println("jwtUtilDebug: " + test);
+        
+        System.out.println("#### END DEBUG ####");
+    }
 }
